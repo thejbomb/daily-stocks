08849f44b1f0afdfd2828dd8b69f319a23080d26
junkun007@aim.com <junkun007@aim.com>
2020-04-19 23:09:02 -0400

    implemented notification and resolved its bugs

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,23 +1,57 @@
 const sendEmail = require('./email');
+const schedule = require('node-schedule');
 
-const handleNotification = (client, connect, stocks) => {
+require('dotenv').config();
+const alpha = require('alphavantage')({key: process.env.ALPHA_VANTAGE_API_KEY});
+const { companies } = require('../constants');
+const { Stock } = require('./Stock');
 
+let stocks = new Map();
+
+//scheduled daily API requests and database lookup
+const sendNotification = (client, connect) => {
+    stocks = new Map();
+
+    schedule.scheduleJob('00 00 12 * * 0-6', function() {
+        companies.forEach(getStockPrices);
+        handleNotification(client, connect);
+    });
+    return stocks;
+}
+
+//API request to alpha for stock prices
+function getStockPrices(company) {
+    alpha.data.quote(company).then(data => {
+        let price = data['Global Quote']['05. price'];
+        let stock = new Stock(company, price);
+        stocks.set(company, stock);
+    })
+    .catch(err => console.log("Too many requests to alpha api"));
+}
+
+//database iteration of whole collection
+const handleNotification = (client, connect) => {
     connect.then(() => {     
         client.db("stocksDb")
         .collection("Users")
-        .find()
-        .then(user => {
-            const msg = handleNotificationMsg(user);
+        .find({}, function(err, users) {
+            if (err) throw err;
 
-            if (msg.length > 0) {
-                sendEmail(user.email, msg)
-            }
+            users.forEach(user => {
+                let msg = '';
+                if (user) {
+                    msg = handleNotificationMsg(user);
+                }
+                if (msg.length > 0) {
+                    sendEmail(user.email, msg)
+                }
+            })
         })
-        .catch(err => console.log(err))
     })
-    .catch(err => console.log('database connection error'))
+    .catch(err => console.log(err))
 }
 
+//compiled msg based on price comparisons
 const handleNotificationMsg = (user) => {
     const incMsgHeader = 'The companies listed have increased by 50% today: \n';
     const decMsgHeader = 'The companies listed have decreased by 50% today: \n';
@@ -30,26 +64,27 @@ const handleNotificationMsg = (user) => {
         const today = stocks.get(name).price, prev = company.price;
 
         if (increase && (prev * inc) === today) {
-            msgInc.append(`${name}\n`)
+            msgInc.concat(`${name}\n`)
         }
         if (decrease && (prev * dec) === today) {
-            msgDec.append(`${name}\n`)
+            msgDec.concat(`${name}\n`)
         }
     })
 
     if (increase && decrease) {
-        msg.append(incMsgHeader + msgInc + '\n' + decMsgHeader + msgDec);
+        msg.concat(incMsgHeader + msgInc + '\n' + decMsgHeader + msgDec);
     }
     else if (increase) {
-        msg.append(incMsgHeader + msgInc);
+        msg.concat(incMsgHeader + msgInc);
     }
     else if (decrease) {
-        msg.append(decMsgHeader + msgDec);
+        msg.concat(decMsgHeader + msgDec);
     }
     
     return msg;
 }
 
 module.exports = {
-    handleNotification: handleNotification
+    handleNotification: handleNotification,
+    sendNotification: sendNotification
 }
--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const schedule = require('node-schedule');
+
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
-const alpha = require('alphavantage')({key: process.env.ALPHA_VANTAGE_API_KEY});
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const notification = require('./controllers/notification');
-
-const { companies } = require('./constants');
-const { Stock } = require('./controllers/Stock');
 
 const client = MongoClient(process.env.MONGODB_URI, {useUnifiedTopology: true});
 
@@ -25,22 +20,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-let stocks = new Map();
-
-function getStockPrices(company) {
-    alpha.data.quote(company).then(data => {
-        let price = data['Global Quote']['05. price'];
-        let stock = new Stock(company, price);
-        stocks.set(company, stock);
-    })
-    .catch(err => console.log("Too many requests to alpha api"));
-}
-
-schedule.scheduleJob('00 00 12 * * 0-6', function() {
-    stocks = new Map();
-    companies.forEach(getStockPrices);
-    notification.handleNotification(client, connect, stocks);
-});
+//daily stocks retreival and notification calls
+let stocks = notification.sendNotification(client, connect);
 
 app.get('/', (req, res)=> { res.send('it is working!') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, client, connect, stocks)});
