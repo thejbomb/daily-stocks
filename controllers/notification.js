1ab92112005080774d20f3375cee7dbe665db6e6
junkun007@aim.com <junkun007@aim.com>
2020-04-28 02:13:13 -0400

    altered time of scheduled task

--- controllers/notification.js
+++ controllers/notification.js
@@ -11,7 +11,7 @@ let stocks = new Map();
 //scheduled daily API requests and database lookup
 const sendNotification = (client, connect) => {
     stocks = new Map();
-    schedule.scheduleJob('*/1 * * * *', function() {
+    schedule.scheduleJob('0 9 * * *', function() {
         companies.forEach(getStockPrices);
         handleNotification(client, connect);
     });
